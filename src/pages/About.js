77a71497b68d12e3ad66b321a1e59056ddb03b57
Aleksandrs Kuskoveckis-Danilovs <52258392+kuskoveckis@users.noreply.github.com>
2021-02-06 21:38:16 +0000

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quaerat nostrum nisi neque assumenda iste non distinctio, laborum ea?
+        Recusandae enim autem error sunt deserunt, molestiae laborum officia fugit, tempore animi commodi provident? Animi, beatae quis mollitia fuga
+        sapiente neque.
+      </p>
+    </section>
+  );
+};
+
+export default About;
